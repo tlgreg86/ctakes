1ca5556abc432d48e07b70e66a747a7ff29d1e41
tmill <tmill@13f79535-47bb-0310-9956-ffa450edef68>
2013-11-05 15:50:36 +0000

    Remove commented out method.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1539038 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-utils/src/main/java/org/apache/ctakes/utils/struct/CounterTreeMap.java
+++ ctakes-utils/src/main/java/org/apache/ctakes/utils/struct/CounterTreeMap.java
@@ -8,7 +8,6 @@ import java.util.TreeMap;
 // and write less boilerplate code.  If you just call it with an object, it will increment the
 // object's count by 1, initializing it to zero first if necessary.
 public class CounterTreeMap<K> {
-
 //	IntValueComparator<K> comp = new IntValueComparator<K>();
 	TreeMap<K,Integer> map = null;
 	
@@ -36,11 +35,6 @@ public class CounterTreeMap<K> {
 	public int size(){
 		return map.size();
 	}
-	
-//	@Override
-//	public int compare(K o1, K o2) {
-//		return o1 - o2;
-//	}
 
 	public Set<K> keySet() {
 		return map.keySet();
