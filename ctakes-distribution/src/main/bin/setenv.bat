70a32c0ade9ad9a1ab4749b91332b0c4673ce198
Vijay Garla <vjapache@apache.org>
2014-01-03 19:33:27 +0000

    increase memory add scripts for ant & ytex
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/branches/ytex@1555220 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-distribution/src/main/bin/setenv.bat
@@ -0,0 +1,44 @@
+@REM
+@REM Licensed to the Apache Software Foundation (ASF) under one
+@REM or more contributor license agreements.  See the NOTICE file
+@REM distributed with this work for additional information
+@REM regarding copyright ownership.  The ASF licenses this file
+@REM to you under the Apache License, Version 2.0 (the
+@REM "License"); you may not use this file except in compliance
+@REM with the License.  You may obtain a copy of the License at
+@REM
+@REM   http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing,
+@REM software distributed under the License is distributed on an
+@REM "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+@REM KIND, either express or implied.  See the License for the
+@REM specific language governing permissions and limitations
+@REM under the License.
+@REM
+
+@rem -------------------------------------------
+@rem customize these variables to match your environment
+@rem -------------------------------------------
+@rem where java JDK is installed
+@set JAVA_HOME=C:\java\jdk1.7.0_25
+
+@rem where ctakes is/will be installed
+@set CTAKES_HOME=C:\java\apache-ctakes-3.1.2-SNAPSHOT
+
+@rem where metamap is installed (optional)
+@set MM_HOME=c:\java\public_mm
+
+@rem use a minimal path
+@set PATH=%JAVA_HOME%\bin;%CTAKES_HOME%\bin;%SystemRoot%;%SystemRoot%\System32;%SystemRoot%\System32\wbem
+
+@rem add 64-bit MS SQL Server authentication library to path
+@rem modify this accordingly if you are using the 32-bit jdk
+@set PATH=%PATH%;%CTAKES_HOME%\lib\auth\x64
+
+@rem -------------------------------------------
+@rem end customizations.  The following is environment-independent
+@rem -------------------------------------------
+
+@set ANT_CP=%CTAKES_HOME%\lib\ant-1.9.2.jar;%CTAKES_HOME%\lib\ant-launcher-1.9.2.jar;%CTAKES_HOME%\lib\ant-contrib-1.0b3.jar
+@set CLASSPATH="%CTAKES_HOME%/lib/*;%CTAKES_HOME%/desc/;%CTAKES_HOME%/resources/"
