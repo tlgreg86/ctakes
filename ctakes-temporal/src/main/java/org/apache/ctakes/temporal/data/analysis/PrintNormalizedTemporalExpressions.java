0d2c47d48fb14dfd608adc3389c481c290eca851
Dmitriy Dligach <dligach@apache.org>
2014-01-27 22:16:48 +0000

    a script useful for checking which temporal expressions in gold data can be normalized by steve bethard's normalizer
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1561884 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-temporal/src/main/java/org/apache/ctakes/temporal/data/analysis/PrintNormalizedTemporalExpressions.java
@@ -0,0 +1,48 @@
+package org.apache.ctakes.temporal.data.analysis;
+
+import org.apache.ctakes.temporal.ae.feature.duration.DurationTimeUnitFeatureExtractor;
+import org.apache.ctakes.typesystem.type.textsem.TimeMention;
+import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.CASException;
+import org.apache.uima.jcas.JCas;
+import org.threeten.bp.temporal.TemporalUnit;
+import org.uimafit.component.JCasAnnotator_ImplBase;
+import org.uimafit.util.JCasUtil;
+
+import scala.collection.immutable.Set;
+
+import com.google.common.collect.Lists;
+
+/**
+ * 
+ */
+public class PrintNormalizedTemporalExpressions extends JCasAnnotator_ImplBase {                                               
+  
+  public static final String GOLD_VIEW_NAME = "GoldView";
+
+  @Override                                                                                                                  
+  public void process(JCas jCas) throws AnalysisEngineProcessException {                                                     
+    
+    JCas goldView;                                                                                                           
+    try {                                                                                                                    
+      goldView = jCas.getView(GOLD_VIEW_NAME);                                                                               
+    } catch (CASException e) {                                                                                               
+      throw new AnalysisEngineProcessException(e);                                                                           
+    }                                                                                                                                                                                                                                         
+    
+    for(TimeMention mention : Lists.newArrayList(JCasUtil.select(goldView, TimeMention.class))) {
+      String timex = mention.getCoveredText();
+      Set<TemporalUnit> units = DurationTimeUnitFeatureExtractor.normalize(timex);
+
+      if(units == null) {
+        System.out.println(timex + "|" + "n/a");
+      } else {
+        scala.collection.Iterator<TemporalUnit> iterator = units.iterator();
+        while(iterator.hasNext()) {
+          TemporalUnit unit = iterator.next();
+          System.out.println(timex + "|" + unit.getName());
+        } 
+      }
+    }
+  }                                                                                                                          
+}                                                                                                                            
