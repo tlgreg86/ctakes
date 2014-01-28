7f652347d38a1563a6d08574a7fd23e8da1d2d8a
Tim Miller <tmill@apache.org>
2014-01-28 15:08:54 +0000

    CTAKES-82: Refactored pipelines, including new pipeline that runs event-time relation annotator with placeholder for event-event relations.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1562089 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-temporal/src/main/java/org/apache/ctakes/temporal/pipelines/FullTemporalExtractionPipeline.java
@@ -0,0 +1,80 @@
+package org.apache.ctakes.temporal.pipelines;
+
+import java.io.File;
+
+import org.apache.ctakes.core.cr.FilesInDirectoryCollectionReader;
+import org.apache.ctakes.temporal.ae.BackwardsTimeAnnotator;
+import org.apache.ctakes.temporal.ae.EventAnnotator;
+import org.apache.ctakes.temporal.ae.EventEventRelationAnnotator;
+import org.apache.ctakes.temporal.ae.EventTimeRelationAnnotator;
+import org.apache.uima.analysis_engine.AnalysisEngine;
+import org.apache.uima.collection.CollectionReader;
+import org.uimafit.component.xwriter.XWriter;
+import org.uimafit.factory.AggregateBuilder;
+import org.uimafit.factory.AnalysisEngineFactory;
+import org.uimafit.factory.CollectionReaderFactory;
+import org.uimafit.pipeline.SimplePipeline;
+
+import com.lexicalscope.jewel.cli.CliFactory;
+import com.lexicalscope.jewel.cli.Option;
+
+public class FullTemporalExtractionPipeline extends
+    TemporalExtractionPipeline_ImplBase {
+
+  static interface FullOptions extends Options {
+    @Option(
+        shortName = "e",
+        description = "specify the path to the directory where the trained event model is located",
+        defaultValue="target/eval/event-spans/train_and_test/")
+    public String getEventModelDirectory();
+    
+    @Option(
+        shortName = "t",
+        description = "specify the path to the directory where the trained event model is located",
+        defaultValue="target/eval/time-spans/train_and_test/BackwardsTimeAnnotator/")
+    public String getTimeModelDirectory();
+    
+    @Option(
+        shortName = "r",
+        description = "Specify the path to the directory where the trained event-time relation model is located",
+        defaultValue="target/eval/temporal-relations/event-time/train_and_test/")
+    public String getEventTimeRelationModelDirectory();
+
+    @Option(
+        shortName = "s",
+        description = "Specify the path to the directory where the trained event-event relation model is located",
+        defaultToNull=true) // add in default value once we have a satisfying trained model
+    public String getEventEventRelationModelDirectory();  
+  }
+
+  /**
+   * @param args
+   * @throws Exception 
+   */
+  public static void main(String[] args) throws Exception {
+    FullOptions options = CliFactory.parseArguments(FullOptions.class, args);
+    
+    CollectionReader collectionReader = CollectionReaderFactory.createCollectionReaderFromPath(
+        "../ctakes-core/desc/collection_reader/FilesInDirectoryCollectionReader.xml",
+        FilesInDirectoryCollectionReader.PARAM_INPUTDIR,
+        options.getInputDirectory());
+
+    AggregateBuilder aggregateBuilder = getPreprocessorAggregateBuilder();
+    aggregateBuilder.add(EventAnnotator.createAnnotatorDescription(new File(options.getEventModelDirectory())));
+    aggregateBuilder.add(BackwardsTimeAnnotator.createAnnotatorDescription(new File(options.getTimeModelDirectory())));
+    aggregateBuilder.add(EventTimeRelationAnnotator.createAnnotatorDescription(new File(options.getEventTimeRelationModelDirectory())));
+    if(options.getEventEventRelationModelDirectory()!=null){
+      aggregateBuilder.add(EventEventRelationAnnotator.createAnnotatorDescription(new File(options.getEventEventRelationModelDirectory())));
+    }
+    AnalysisEngine xWriter = AnalysisEngineFactory.createPrimitive(
+        XWriter.class,
+        XWriter.PARAM_OUTPUT_DIRECTORY_NAME,
+        options.getOutputDirectory());
+    
+    SimplePipeline.runPipeline(
+        collectionReader,
+        aggregateBuilder.createAggregate(),
+        xWriter);
+  }
+
+}
