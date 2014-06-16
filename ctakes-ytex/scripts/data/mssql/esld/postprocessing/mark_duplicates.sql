455d0b47bce7a8a313b82509b175cbb1df152784
Pei Chen <chenpei@apache.org>
2014-06-16 15:32:43 +0000

    CTAKES-301 - Create a cTAKES 3.2.0 Release Applying ASF License Headers onto ytex project
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1602900 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-ytex/scripts/data/mssql/esld/postprocessing/mark_duplicates.sql
+++ ctakes-ytex/scripts/data/mssql/esld/postprocessing/mark_duplicates.sql
@@ -1,3 +1,22 @@
+--
+-- Licensed to the Apache Software Foundation (ASF) under one
+-- or more contributor license agreements.  See the NOTICE file
+-- distributed with this work for additional information
+-- regarding copyright ownership.  The ASF licenses this file
+-- to you under the Apache License, Version 2.0 (the
+-- "License"); you may not use this file except in compliance
+-- with the License.  You may obtain a copy of the License at
+--
+--   http://www.apache.org/licenses/LICENSE-2.0
+--
+-- Unless required by applicable law or agreed to in writing,
+-- software distributed under the License is distributed on an
+-- "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+-- KIND, either express or implied.  See the License for the
+-- specific language governing permissions and limitations
+-- under the License.
+--
+
 -- documents for the same subject with the same date that are identical are marked as duplicates
 -- for each document for the same subject on the same day
 -- compare the sum of sentence lengths of each sentence after the 'Report:' string
