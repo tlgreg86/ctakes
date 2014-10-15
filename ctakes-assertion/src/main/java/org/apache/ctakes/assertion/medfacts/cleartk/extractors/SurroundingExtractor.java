aa027d4476e19e1bfa816e76b64a69553a3bb44b
Tim Miller <tmill@apache.org>
2014-10-15 14:03:51 +0000

    Remove references to zoner.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1632045 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-assertion/src/main/java/org/apache/ctakes/assertion/medfacts/cleartk/extractors/SurroundingExtractor.java
+++ ctakes-assertion/src/main/java/org/apache/ctakes/assertion/medfacts/cleartk/extractors/SurroundingExtractor.java
@@ -17,13 +17,12 @@
  * under the License.
  */
 package org.apache.ctakes.assertion.medfacts.cleartk.extractors;
-
+//
 import java.util.ArrayList;
-import java.util.Collection;
+//import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ctakes.assertion.zoner.types.Zone;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
 import org.apache.log4j.Logger;
 import org.apache.uima.fit.util.JCasUtil;
@@ -35,47 +34,47 @@ import org.cleartk.ml.feature.extractor.FeatureExtractor1;
 
 public class SurroundingExtractor implements FeatureExtractor1
 {
-	protected static final Logger logger = Logger.getLogger(SurroundingExtractor.class);
-	
-//	Class<? extends Annotation> ancestorAnnotationClass;
+//	protected static final Logger logger = Logger.getLogger(SurroundingExtractor.class);
 //	
-//	public SurroundingExtractor(Class<? extends Annotation> ancestorAnnotationClass)
-//	{
-//		this.ancestorAnnotationClass = ancestorAnnotationClass;
-//	}
-
+////	Class<? extends Annotation> ancestorAnnotationClass;
+////	
+////	public SurroundingExtractor(Class<? extends Annotation> ancestorAnnotationClass)
+////	{
+////		this.ancestorAnnotationClass = ancestorAnnotationClass;
+////	}
+//
 	@Override
 	public List<Feature> extract(JCas jcas, Annotation targetAnnotation)
 			throws CleartkExtractorException
 	{
-		logger.info("SurroundingExtractor.extract() BEGIN");
-		//JCasUtil.indexCovering(jcas, IdentifiedAnnotation.class, this.ancestorAnnotationClass)
-		
-		Map<IdentifiedAnnotation, Collection<Zone>> coveringMap =
-			JCasUtil.indexCovering(jcas, IdentifiedAnnotation.class, Zone.class);
-		
-		IdentifiedAnnotation targetEntityAnnotation = (IdentifiedAnnotation)targetAnnotation;
-		
-		Collection<Zone> zoneList = coveringMap.get(targetEntityAnnotation);
-		
-		if (zoneList == null || zoneList.isEmpty())
-		{
-			//return null;
-			logger.info("SurroundingExtractor.extract() early END (no zones)");
-			new ArrayList<Feature>();
-		}
-		
+//		logger.info("SurroundingExtractor.extract() BEGIN");
+//		//JCasUtil.indexCovering(jcas, IdentifiedAnnotation.class, this.ancestorAnnotationClass)
+//		
+//		Map<IdentifiedAnnotation, Collection<Zone>> coveringMap =
+//			JCasUtil.indexCovering(jcas, IdentifiedAnnotation.class, Zone.class);
+//		
+//		IdentifiedAnnotation targetEntityAnnotation = (IdentifiedAnnotation)targetAnnotation;
+//		
+//		Collection<Zone> zoneList = coveringMap.get(targetEntityAnnotation);
+//		
+//		if (zoneList == null || zoneList.isEmpty())
+//		{
+//			//return null;
+//			logger.info("SurroundingExtractor.extract() early END (no zones)");
+//			new ArrayList<Feature>();
+//		}
+//		
 		ArrayList<Feature> featureList = new ArrayList<Feature>();
-		for (Zone zone : zoneList)
-		{
-			Feature currentFeature = new Feature("zone", zone.getLabel());
-			logger.info(String.format("zone: %s", zone.getLabel()));
-			logger.info(String.format("zone feature: %s", currentFeature.toString()));
-			featureList.add(currentFeature);
-		}
-		
-		logger.debug("SurroundingExtractor.extract() END");
+//		for (Zone zone : zoneList)
+//		{
+//			Feature currentFeature = new Feature("zone", zone.getLabel());
+//			logger.info(String.format("zone: %s", zone.getLabel()));
+//			logger.info(String.format("zone feature: %s", currentFeature.toString()));
+//			featureList.add(currentFeature);
+//		}
+//		
+//		logger.debug("SurroundingExtractor.extract() END");
 		return featureList;
 	}
-
+//
 }
