cdea7e15e8af130a9a83be7a748fff7b9478950d
Chen Lin <clin@apache.org>
2015-08-06 20:44:50 +0000

    enable generating more training instances by event-expansion technique for "location of" relations. To do expansion, add "--expand-events" in command line arguments.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1694571 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-relation-extractor/src/main/java/org/apache/ctakes/relationextractor/ae/LocationOfRelationExtractorAnnotator.java
+++ ctakes-relation-extractor/src/main/java/org/apache/ctakes/relationextractor/ae/LocationOfRelationExtractorAnnotator.java
@@ -19,12 +19,16 @@
 package org.apache.ctakes.relationextractor.ae;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.ctakes.relationextractor.eval.RelationExtractorEvaluation;
 import org.apache.ctakes.typesystem.type.relation.BinaryTextRelation;
 import org.apache.ctakes.typesystem.type.relation.LocationOfTextRelation;
 import org.apache.ctakes.typesystem.type.relation.RelationArgument;
 import org.apache.ctakes.typesystem.type.textsem.AnatomicalSiteMention;
+import org.apache.ctakes.typesystem.type.textsem.EntityMention;
 import org.apache.ctakes.typesystem.type.textsem.EventMention;
 import org.apache.ctakes.typesystem.type.textsem.IdentifiedAnnotation;
 import org.apache.ctakes.typesystem.type.textspan.Sentence;
@@ -38,53 +42,103 @@ import org.apache.uima.fit.util.JCasUtil;
  */
 public class LocationOfRelationExtractorAnnotator extends RelationExtractorAnnotator {
 
-  @Override
-  protected Class<? extends BinaryTextRelation> getRelationClass() {
-    return LocationOfTextRelation.class;
-  }
+	@Override
+	protected Class<? extends BinaryTextRelation> getRelationClass() {
+		return LocationOfTextRelation.class;
+	}
 
-  @Override
-  public List<IdentifiedAnnotationPair> getCandidateRelationArgumentPairs(
-      JCas identifiedAnnotationView,
-      Annotation sentence) {
+	@Override
+	public List<IdentifiedAnnotationPair> getCandidateRelationArgumentPairs(
+			JCas identifiedAnnotationView,
+			Annotation sentence) {
+		
+		List<EventMention> events =
+				JCasUtil.selectCovered(identifiedAnnotationView, EventMention.class, sentence);
+		List<AnatomicalSiteMention> sites =
+				JCasUtil.selectCovered(identifiedAnnotationView, AnatomicalSiteMention.class, sentence);
 
-    List<EventMention> events =
-        JCasUtil.selectCovered(identifiedAnnotationView, EventMention.class, sentence);
-    List<AnatomicalSiteMention> sites =
-        JCasUtil.selectCovered(identifiedAnnotationView, AnatomicalSiteMention.class, sentence);
+		List<IdentifiedAnnotationPair> pairs = new ArrayList<>();
+		
+		if(RelationExtractorEvaluation.expandEvent){//if expand
+			Map<EventMention, Collection<EventMention>> coveredMap =
+					JCasUtil.indexCovered(identifiedAnnotationView, EventMention.class, EventMention.class);
+			Map<EventMention, Collection<EventMention>> coveringMap =
+					JCasUtil.indexCovering(identifiedAnnotationView, EventMention.class, EventMention.class);
+			Map<AnatomicalSiteMention, Collection<EventMention>> siteEventMap =
+					JCasUtil.indexCovered(identifiedAnnotationView, AnatomicalSiteMention.class, EventMention.class);
+			Map<AnatomicalSiteMention, Collection<EntityMention>> siteEntityMap =
+					JCasUtil.indexCovering(identifiedAnnotationView, AnatomicalSiteMention.class, EntityMention.class);
+			
+			final List<IdentifiedAnnotation> eventList = new ArrayList<>();
+			for (EventMention event : events) {
+				eventList.addAll(coveringMap.get(event));
+				eventList.addAll(coveredMap.get(event));
+				for(IdentifiedAnnotation covEvent : eventList){
+					for (AnatomicalSiteMention site : sites) {
+						if(!hasOverlap(covEvent,site)){
+							pairs.add(new IdentifiedAnnotationPair(covEvent, site));
+						}
+					}
+				}
+				eventList.clear();
+				for (AnatomicalSiteMention site : sites) {
+					pairs.add(new IdentifiedAnnotationPair(event, site));
+					eventList.addAll(siteEventMap.get(site));
+					eventList.addAll(siteEntityMap.get(site));
+					for(IdentifiedAnnotation covSite : eventList){
+						if(!hasOverlap(event,covSite)){
+							pairs.add(new IdentifiedAnnotationPair(event, covSite));
+						}
+					}
+					eventList.clear();
+				}
+				
+			}
+		}else{//id don't expand
+			for (EventMention event : events) {
+				for (AnatomicalSiteMention site : sites) {
+					pairs.add(new IdentifiedAnnotationPair(event, site));
+				}
+			}
+		}
+		
+		
+		return pairs;
+	}
+	
+	private static boolean hasOverlap(Annotation event1, Annotation event2) {
+		if(event1.getEnd()>=event2.getBegin()&&event1.getEnd()<=event2.getEnd()){
+			return true;
+		}
+		if(event2.getEnd()>=event1.getBegin()&&event2.getEnd()<=event1.getEnd()){
+			return true;
+		}
+		return false;
+	}
 
-    List<IdentifiedAnnotationPair> pairs = new ArrayList<IdentifiedAnnotationPair>();
-    for (EventMention event : events) {
-      for (AnatomicalSiteMention site : sites) {
-        pairs.add(new IdentifiedAnnotationPair(event, site));
-      }
-    }
-    return pairs;
-  }
+	@Override
+	protected void createRelation(
+			JCas jCas,
+			IdentifiedAnnotation arg1,
+			IdentifiedAnnotation arg2,
+			String predictedCategory) {
+		RelationArgument relArg1 = new RelationArgument(jCas);
+		relArg1.setArgument(arg1);
+		relArg1.setRole("Argument");
+		relArg1.addToIndexes();
+		RelationArgument relArg2 = new RelationArgument(jCas);
+		relArg2.setArgument(arg2);
+		relArg2.setRole("Related_to");
+		relArg2.addToIndexes();
+		LocationOfTextRelation relation = new LocationOfTextRelation(jCas);
+		relation.setArg1(relArg1);
+		relation.setArg2(relArg2);
+		relation.setCategory(predictedCategory);
+		relation.addToIndexes();
+	}
 
-  @Override
-  protected void createRelation(
-      JCas jCas,
-      IdentifiedAnnotation arg1,
-      IdentifiedAnnotation arg2,
-      String predictedCategory) {
-    RelationArgument relArg1 = new RelationArgument(jCas);
-    relArg1.setArgument(arg1);
-    relArg1.setRole("Argument");
-    relArg1.addToIndexes();
-    RelationArgument relArg2 = new RelationArgument(jCas);
-    relArg2.setArgument(arg2);
-    relArg2.setRole("Related_to");
-    relArg2.addToIndexes();
-    LocationOfTextRelation relation = new LocationOfTextRelation(jCas);
-    relation.setArg1(relArg1);
-    relation.setArg2(relArg2);
-    relation.setCategory(predictedCategory);
-    relation.addToIndexes();
-  }
-
-  @Override
-  protected Class<? extends Annotation> getCoveringClass() {
-    return Sentence.class;
-  }
+	@Override
+	protected Class<? extends Annotation> getCoveringClass() {
+		return Sentence.class;
+	}
 }
