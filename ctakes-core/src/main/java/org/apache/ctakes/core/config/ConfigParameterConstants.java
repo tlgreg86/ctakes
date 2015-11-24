e3d87290de3970ee9aef3307c5c5c0cd1729181a
Sean Finan <seanfinan@apache.org>
2015-11-24 23:41:36 +0000

    Missing from checkin, recent refactor/extraction
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1716279 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-core/src/main/java/org/apache/ctakes/core/config/ConfigParameterConstants.java
@@ -0,0 +1,23 @@
+package org.apache.ctakes.core.config;
+
+/**
+ * @author SPF , chip-nlp
+ * @version %I%
+ * @since 9/28/2015
+ */
+public interface ConfigParameterConstants {
+
+
+   static public final String PARAM_INPUTDIR = "InputDirectory";
+   static public final String PARAM_OUTPUTDIR = "OutputDirectory";
+
+
+   static public final String OPTION_INPUTDIR = "-i";
+   static public final String OPTION_OUTPUTDIR = "-o";
+
+
+   static public final String DESC_INPUTDIR = "Directory for all input files";
+   static public final String DESC_OUTPUTDIR = "Directory for all output files";
+
+
+}
