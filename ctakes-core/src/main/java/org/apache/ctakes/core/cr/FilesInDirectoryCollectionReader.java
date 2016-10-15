69b4e93ad0c96015cfd2b61e27cf75f9b079ba00
Sean Finan <seanfinan@apache.org>
2016-10-15 00:53:39 +0000

    FilesInDirectoryCollectionReader now uses FileLocator to access resources in source Adding Example that reads files from a directory using PipelineBuilder moved ctakes-examples/data/ to a ctakes-examples-res module Added all other sample notes in ctakes project to the ctakes-examples-res module    TODO refactor all test and sample code in ctakes to use ctakes-examples-res sample notes
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1765001 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-core/src/main/java/org/apache/ctakes/core/cr/FilesInDirectoryCollectionReader.java
+++ ctakes-core/src/main/java/org/apache/ctakes/core/cr/FilesInDirectoryCollectionReader.java
@@ -39,28 +39,23 @@ package org.apache.ctakes.core.cr;
  * gather files in the directory from sub directories.    
  */
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-
+import org.apache.ctakes.core.resource.FileLocator;
+import org.apache.ctakes.typesystem.type.structured.DocumentID;
 import org.apache.uima.cas.CAS;
 import org.apache.uima.cas.CASException;
 import org.apache.uima.collection.CollectionException;
 import org.apache.uima.collection.CollectionReader_ImplBase;
 import org.apache.uima.jcas.JCas;
-//import org.apache.uima.jcas.tcas.DocumentAnnotation;
 import org.apache.uima.resource.ResourceConfigurationException;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.Progress;
 import org.apache.uima.util.ProgressImpl;
 
-import org.apache.ctakes.typesystem.type.structured.DocumentID;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+//import org.apache.uima.jcas.tcas.DocumentAnnotation;
 
 public class FilesInDirectoryCollectionReader extends CollectionReader_ImplBase 
 {
@@ -107,11 +102,20 @@ public class FilesInDirectoryCollectionReader extends CollectionReader_ImplBase
 	  /**
 	   * @see org.apache.uima.collection.CollectionReader_ImplBase#initialize()
 	   */
+	  @Override
 	public void initialize() throws ResourceInitializationException
 	{
-	    File directory = new File((String)getConfigParameterValue(PARAM_INPUTDIR));
-	    iv_encoding = (String)getConfigParameterValue(PARAM_ENCODING);
-	    iv_language = (String)getConfigParameterValue(PARAM_LANGUAGE);
+		final String inputDirPath = (String)getConfigParameterValue( PARAM_INPUTDIR );
+		File directory;
+		try {
+			directory = FileLocator.locateFile( inputDirPath );
+		} catch ( IOException ioE ) {
+			throw new ResourceInitializationException(
+					ResourceConfigurationException.DIRECTORY_NOT_FOUND,
+					new Object[] { PARAM_INPUTDIR, getMetaData().getName(), inputDirPath } );
+		}
+		iv_encoding = (String)getConfigParameterValue( PARAM_ENCODING );
+		iv_language = (String)getConfigParameterValue(PARAM_LANGUAGE);
 	    iv_extensions = (String[]) getConfigParameterValue(PARAM_EXTENSIONS);
 	    
 	    iv_currentIndex = 0;
