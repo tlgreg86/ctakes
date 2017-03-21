109ace689e7ecc4c35555861beb1c2fad563d3a8
Tim Miller <tmill@apache.org>
2017-03-21 17:05:27 +0000

    Initial version of character-based sentence annotator. Slightly slower but doesn't break on newlines.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1788006 13f79535-47bb-0310-9956-ffa450edef68

---
+++ ctakes-core/src/main/java/org/apache/ctakes/core/pipeline/GenerateSentenceDescriptors.java
@@ -0,0 +1,26 @@
+package org.apache.ctakes.core.pipeline;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+import org.apache.ctakes.core.ae.SentenceDetectorAnnotator;
+import org.apache.ctakes.core.ae.SimpleSegmentAnnotator;
+import org.apache.uima.fit.factory.AggregateBuilder;
+import org.apache.uima.resource.ResourceInitializationException;
+import org.xml.sax.SAXException;
+
+public class GenerateSentenceDescriptors {
+
+  public static final String sentModelPath = "/org/apache/ctakes/core/sentdetect/model.jar";
+  public static final String segModelPath = "/org/apache/ctakes/core/segdetect/model.jar";
+  
+  public static void main(String[] args) throws ResourceInitializationException, SAXException, IOException {
+    AggregateBuilder aggregateBuilder = new AggregateBuilder();
+    aggregateBuilder.add(SimpleSegmentAnnotator.createAnnotatorDescription());
+    aggregateBuilder.add(SentenceDetectorAnnotator.getDescription(sentModelPath));
+    
+    aggregateBuilder.createAggregateDescription().toXML(new FileWriter("desc/analysis_engine/SentenceAnnotatorAggregate.xml"));
+    SentenceDetectorAnnotator.getDescription(sentModelPath).toXML(new FileWriter("desc/analysis_engine/SentenceAnnotator.xml"));    
+  }
+
+}
