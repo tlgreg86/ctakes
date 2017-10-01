8288e259766cc639ac5e17ef804455dc64e306b2
James Joseph Masanz <james-masanz@apache.org>
2017-10-01 18:18:28 +0000

    CTAKES-445 - improvement but not complete fix.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1810271 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-lvg/src/main/java/org/apache/ctakes/lvg/resource/LvgCmdApiResourceImpl.java
+++ ctakes-lvg/src/main/java/org/apache/ctakes/lvg/resource/LvgCmdApiResourceImpl.java
@@ -62,7 +62,7 @@ public class LvgCmdApiResourceImpl
     	try
     	{
     		
-	    	File configFile = new File(dr.getUri());
+    		File configFile = new File(dr.getUrl().toExternalForm());
 	        configFileName = configFile.getPath();
 	         
 	        logger.info("Loading NLM Norm and Lvg with config file = " + configFileName);
