8d6332d435673ea2ea780d564153b780058481aa
Tim Miller <tmill@apache.org>
2018-05-08 00:10:08 +0000

    Undo bad import of wrong Segment class.
    git-svn-id: https://svn.apache.org/repos/asf/ctakes/trunk@1831139 13f79535-47bb-0310-9956-ffa450edef68

--- ctakes-core/src/main/java/org/apache/ctakes/core/ae/SentenceDetector.java
+++ ctakes-core/src/main/java/org/apache/ctakes/core/ae/SentenceDetector.java
@@ -43,7 +43,6 @@ import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
 import org.apache.uima.resource.ResourceInitializationException;
 
-import javax.swing.text.Segment;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
